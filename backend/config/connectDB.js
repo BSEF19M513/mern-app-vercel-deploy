c83f8249f47ed84bd3e7208623b6fe51cda9a3cd
BSEF19M513 <72209038+BSEF19M513@users.noreply.github.com>
2024-03-21 01:00:00 +0500

    Update connectDB.js

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
     try {
-        const mongoURI = 'mongodb://localhost:27017/mernstack';
+        const mongoURI = 'mongodb+srv://pakman4990:pakman4990@cluster0.6tnmxxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
         await mongoose.connect(mongoURI);
         console.log('Connected to MongoDB');
     } catch (error) {
